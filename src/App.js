33693905b5c2fe826eeb0fa2f7de15a42ba3dd7a
Anat01iy <tolikushtan24@gmail.com>
2023-02-15 19:05:15 +0200

    chanhed something

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
-import Card from './components/Card'
+import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
+
+const arr = [
+  {title: "Кроссовки Puma X Aka Boku Future Rider", 
+  price: 12999,
+  imageUrl: "/img/sneakers/s1.jpg"
+},
+  {title: "Мужские Кроссовки Nike Air Max 270", 
+  price: 15600,
+  imageUrl: "/img/sneakers/s2.jpg"
+},
+  {title: "Мужские Кроссовки Nike Blazer Mid Suede", 
+  price: 17600,
+  imageUrl: "/img/sneakers/s3.jpg"
+},
+  {title: "Мужские Кроссовки Under Armour Curry 8", 
+  price: 23800,
+  imageUrl: "/img/sneakers/s4.jpg"
+},
+  {title: "Мужские Кроссовки Nike Lebron XVIII Low", 
+  price: 13800,
+  imageUrl: "/img/sneakers/s5.jpg"
+},
+];
 
 function App() {
   return (
@@ -15,12 +38,13 @@ function App() {
           </div>
         </div>  
         <div className="cardBox">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {arr.map((obj) => (
+              <Card title={obj.title}
+              price={obj.price} 
+              imageUrl={obj.imageUrl}
+              onClick={() => console.log(obj)}
+              />
+          ))}
         </div>
       </div>
     </div>
